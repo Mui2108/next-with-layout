2ed6f3b38ba6edad1b27be06631dde0819c54674
ภานุพงศ์ อินทริง <mooy.panupong21@gmail.com>
2023-01-19 12:30:51 +0700

    Update app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,18 +3,22 @@ import type { AppProps } from "next/app";
 import "../styles/customize-theme-antd/variables.less";
 import "../styles/antd/index.less";
 import Layouts from "../layout/Layouts";
-import Login from "../components/Login";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
 function MyApp({ Component, pageProps }: AppProps) {
-  const token = "ddd";
-  if (token) {
-    return (
-      <Layouts>
-        <Component {...pageProps} />
-      </Layouts>
-    );
-  } else {
-    return <Login />;
-  }
+  const { pathname, push } = useRouter();
+  useEffect(() => {
+    const token = sessionStorage.getItem("token");
+    if (!token) push("/login");
+  }, [pathname]);
+
+  if (pathname === "/login") return <Component {...pageProps} />;
+  return (
+    <Layouts>
+      <Component {...pageProps} />
+    </Layouts>
+  );
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,20 @@
 import type { NextPage } from "next";
 
 import { Button } from "antd";
+import { useEffect } from "react";
+import useAuth from "../components/hooks/auth/useAuth";
+
 const Home: NextPage = () => {
+  const { GET_ME } = useAuth();
+  useEffect(() => {
+    getMe();
+  }, []);
+
+  const getMe = async () => {
+    try {
+      const res = await GET_ME();
+    } catch (error) {}
+  };
   return <Button type="primary">Click</Button>;
 };
 
